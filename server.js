632a67e5afa06168d34280b441e6f8d7f3734d38
ollie <ollie@bubblegumhedge>
2012-12-17 09:36:34 +0000

    Added IRC passcode notification (but broke portals)

--- server.js
+++ server.js
@@ -2,8 +2,53 @@ var Portal = require("./lib/portal");
 var Passcode = require("./lib/passcode");
 var color = require("ansi-color").set;
 var config = require("./config.json");
+var irc = require('irc');
 
 var collectedPasscodes = {};
+var passcodeOutput = [];
+
+config.passcode.output.forEach(function(output){
+	if (output.type === "irc") {
+		var client = new irc.Client(
+			output.host,
+			output.user,
+			{
+				channels: output.channels,
+				port: output.port,
+				selfSigned: true,
+				secure: output.ssl,
+				password: output.password,
+				certExpired: true
+			}
+		);
+			
+		client.addListener('error', function(message) {
+			console.error('ERROR: %s: %s', message.command, message.args.join(' '));
+		});
+		
+		client.addListener('message', function (from, to, message) {
+			if (message === "passcode search now") {
+				getPasscodes();
+			}
+			output.channels.forEach(function(channel) {
+				client.say(channel, "Fetching any passcodes " + from);
+			});
+		});
+			
+		setTimeout(function(){
+			output.channels.forEach(function(channel) {
+				client.say(channel, "I'm alive!");
+			});
+		}, 20 * 1000);
+		
+		passcodeOutput.push({
+			type: "irc",
+			connection: client
+		});
+
+	}	
+	
+});
 
 function getPasscodes() {
 	Passcode.fetchAll(function(passcodes){
@@ -11,6 +56,13 @@ function getPasscodes() {
 			console.log("Passcode: " + passcode);
 			if (collectedPasscodes[passcode] === undefined) {
 				console.log(new Date().getUTCDate() + " - NEW CODE!!!!! " + passcode);
+				passcodeOutput.forEach(function(output){
+					if (output.type === "irc") {
+						output.channels.forEach(function(channel) {
+							client.say(channel, "#PASSCODE: " + passcode);
+						});
+					}
+				});
 			} else {
 				collectedPasscodes[passcode] = true;
 			}
@@ -47,9 +99,12 @@ function getPortals() {
 		});
 	});
 }
-
-//getPortals();
-
 setInterval(function(){
 	getPasscodes();
 }, (config.passcode.interval === undefined ? 30 : config.passcode.interval) * 1000);
+
+
+setInterval(function(){
+	getPortals();
+}, 5 * 60 * 1000);
+getPortals();
